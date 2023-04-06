feb90ef2773c6d47ed2940e6e6d167b529cfb2ef
mikoycc <54666324+mikoycc@users.noreply.github.com>
2023-04-06 18:48:08 +0800

    Update Nicegram.js
    Signed-off-by: mikoycc <54666324+mikoycc@users.noreply.github.com>

--- FX/Nicegram.js
+++ FX/Nicegram.js
@@ -1,7 +1,6 @@
 [mitm] 
 hostname=restore-access.indream.app
 
-*******************************
 
 [rewrite_local]
 https?:\/\/restore-access\.indream\.app\/restoreAccess\?id=\d{5,10} url echo-response text/json echo-response https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Nicegram.js

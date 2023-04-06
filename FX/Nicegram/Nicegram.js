17b6289279359711f15a953bfbb9c22c2b57b3dc
mikoycc <54666324+mikoycc@users.noreply.github.com>
2023-04-06 19:59:44 +0800

    Update Nicegram.js
    Signed-off-by: mikoycc <54666324+mikoycc@users.noreply.github.com>

--- FX/Nicegram/Nicegram.js
+++ FX/Nicegram/Nicegram.js
@@ -1,6 +1,10 @@
+# 脚本作者：彭于晏💞
+# 更新时间：2022-10-8
+# TG频道群：https://t.me/py996
+
+
 [mitm] 
 hostname=restore-access.indream.app
-
 
 [rewrite_local]
 https?:\/\/restore-access\.indream\.app\/restoreAccess\?id=\d{5,10} url echo-response text/json echo-response https://raw.githubusercontent.com/mikoycc/QuantumultX/main/FX/Nicegram/ng.js

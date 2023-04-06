5ca74a4c67d836212d6c19cf25bb2310194f72a6
mikoycc <54666324+mikoycc@users.noreply.github.com>
2023-04-06 22:26:01 +0800

    Update aiqicha.js
    Signed-off-by: mikoycc <54666324+mikoycc@users.noreply.github.com>

--- FX/AiQiCha/aiqicha.js
+++ FX/AiQiCha/aiqicha.js
@@ -4,6 +4,7 @@
 mitm= aiqicha.baidu.com
 */
 
+
 var body = $response.body
     .replace(/\"svip\":\{\"status\":0,\"startTime\":\"\",\"endTime\":\"\"/, "\"svip\":\{\"status\":1,\"startTime\":\"\",\"endTime\":\"2099-12-31\"");
 $done({ body });
